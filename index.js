e2224c8f9215f982123c7b0fd4f0ea721e393acd
David Brennan <ddbren@gmail.com>
2017-10-13 20:27:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
+  return object
 }
 
 function deleteFromObjectByKey(object, key) {
@@ -14,4 +15,5 @@ function deleteFromObjectByKey(object, key) {
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
+  return object
 }
